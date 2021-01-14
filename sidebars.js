312a40cc69360a9a22a01ceb23f3f6f66f3fc450
nikolqy <76059737+nikolqy@users.noreply.github.com>
2021-01-13 22:51:36 -0600

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3', '/Minecraft/doc1'],
-    Features: ['mdx'],
+    Docusaurus: ['doc1', 'doc2', 'doc3', 'doc4', 'doc5'],
+    Minecraft: ['doc1'],
+    CloudFlare: ['cf'],
   },
 };
